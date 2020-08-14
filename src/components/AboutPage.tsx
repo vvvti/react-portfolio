a64c0a0624a3d92964a0b97a6ed7d01c0f8a0cc5
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-08-14 23:29:56 +0200

    AboutPage component

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
 
-const AboutPage = () => {
+export const AboutPage: React.FC = () => {
     return (
         <div>
           <h1 className="lg-heading">
             About me
           </h1>
           <h2 className="sm-heading">A few words about me...</h2>
+          <div className="about-info">
+            <img src="/" alt="Patryk Kontewicz" className="bio-image"/>
+            <div className="bio">
+              <h3 className="text-secondary">BIO</h3>
+              <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Voluptatibus ipsa, sed cupiditate numquam repellendus at 
+                corporis quo ullam eligendi beatae libero perferendis saepe 
+                temporibus praesentium consectetur placeat modi aspernatur 
+                explicabo?</p>
+                <div className="skills"></div>
+                <div className="skills"></div>
+                <div className="skills"></div>
+            </div>
+          </div>
         </div>
     )
 }
