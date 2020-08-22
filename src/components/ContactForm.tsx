5743318a97d01760ecf10212d168780026ebcdef
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-08-22 23:01:09 +0200

    Add ContactForm component and scss

---
+++ src/components/ContactForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const ContactForm: React.FC = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ContactForm
---
+++ src/components/MenuDrawerButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const MenuDrawerButton: React.FC = () => {
+    return (
+        <button className="drawer-button">
+            <div className="line"></div>
+            <div className="line"></div>
+            <div className="line"></div>            
+        </button>
+    )
+}
+
+export default MenuDrawerButton
