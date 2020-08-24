70d4a3dadfbc3a7c17f6ddbb3c5784d0b909b8d9
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-08-24 22:01:56 +0200

    Add state to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import MenuDrawer from './components/MenuDrawer';
 
 import './App.css';
 
 export const App: React.FC = () => {
+  const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
   return (
     <div className="App">
+      <MenuDrawer {isOpen, menuOpenHandler}/>
       <Header />
       <HomePage />
       
