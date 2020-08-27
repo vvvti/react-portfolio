d78d9dc8be0f5843a6a42903ec7913bb3a4a6814
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-08-27 22:02:41 +0200

    Install types for react-router-dom and setup routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import React, {useState} from 'react';
+import React, {Fragment, useState} from 'react';
+
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import MenuDrawer from './components/MenuDrawer';
-import Footer from './components/Footer'
+import Footer from './components/Footer';
+import AboutPage from './components/AboutPage';
+import ProjectsPage from './components/ProjectsPage';
 
 import './App.css';
+import ContactForm from './components/ContactForm';
+
 
 
 export const App: React.FC = () => {
-  const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
+  const [menuDrawerOpen, setMenuDrawerOpen] = useState<boolean>(false);
 
   const menuOpenHandler = () => {
       setMenuDrawerOpen(!menuDrawerOpen);
@@ -22,15 +32,22 @@ export const App: React.FC = () => {
   }
 
   return (
+    <Router>
+    <Fragment>
     <div className="App">
-      {menuDrawer}
-      
+      {menuDrawer}      
       <Header />
-      <HomePage />
-      <Footer />
-      
-     <h1>Portfolio</h1>
-    </div>
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/AboutPage" component={AboutPage} />
+      <Route exact path="/Projects" component={ProjectsPage} />
+      <Route exact path="/Contact" component={ContactForm} />
+      <HomePage />   
+     </Switch>
+    </div>    
+    <Footer />
+    </Fragment>
+    </Router>
   );
 }
 
