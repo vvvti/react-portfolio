5a502c8c5dbad5de0b1881a82d929835d5130c39
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-08-28 23:11:19 +0200

    JSx for ProjectsPage component

--- src/components/AboutPage.tsx
+++ src/components/AboutPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import './AboutPage.css'
 
 export const AboutPage: React.FC = () => {
     return (
--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 export const ProjectsPage: React.FC = () => {
     return (
-        <div>
+        <div className="projects-info">
+            <div className="web-apps">
+                <h3>ALLorganizer</h3>
+                <h3>TSLHub</h3>
+                <h3>XKCDbrowser</h3>
+            </div>
+            <div className="landing-pages">
+                <h3>Ecommerce</h3>
+                <h3>Landing 2</h3>
+            </div>
+            <div className="other-projects">
+                <h3>RPi & XBee </h3>
+            </div>
+
             
         </div>
     )
