0a45df15ee43d398981fa2409b684207bc966598
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-09-08 23:08:35 +0200

    Add html and sass for ProjectsPage component

--- src/components/ProjectsPage.tsx
+++ src/components/ProjectsPage.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
+import './ProjectsPage.css'
+
 export const ProjectsPage: React.FC = () => {
     return (
-        <section className="projects-info">
-            <div className="projects">
-            <div className="web-apps">
+      <section className="projects-info">
+          <h1 className="lg-heading">
+            Some sample<span> projects</span>
+          </h1>
+          <h2 className="sm-heading">A few words about me...</h2>        
+              <div className="web-apps">
                 <h3>ALLorganizer</h3>
                 <h3>TSLHub</h3>
                 <h3>XKCDbrowser</h3>
-            </div>
+              </div>
             <div className="landing-pages">
                 <h3>Ecommerce</h3>
                 <h3>Landing 2</h3>
             </div>
-            <div className="other-projects">
+              <div className="other-projects">
                 <h3>RPi & XBee </h3>
-            </div>
-            </div>
-
-            
-        </section>
+             </div>
+         </section>
     )
 }
 
