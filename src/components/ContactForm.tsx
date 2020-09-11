f81b58d720ba2d925c55e59b079b5295d42937be
Patryk Kontewicz <patryk.kontewicz@poczta.fm>
2020-09-11 23:26:51 +0200

    Add html to Contactpage component

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -4,7 +4,18 @@ import './ContactForm.css'
 export const ContactForm: React.FC = () => {
     return (
         <section className='contact-page'>
-            
+            <div>
+                <span className="text-secondary">Email:</span>
+                patryk.kontewicz@poczta.fm
+            </div>
+            <div>
+                <span className="text-secondary">Phone:</span>
+                666 666 666
+            </div>
+            <div>
+                <span className="text-secondary">Address:</span>
+                Nowy Sacz, Poland
+            </div>
         </section>
     )
 }
